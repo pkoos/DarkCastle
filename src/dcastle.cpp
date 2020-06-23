5f51a65c6c99cb67e4801827b2c231a78c4562a2
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2020-06-23 00:17:46 -0500

    WIP changed arg parsing

--- src/dcastle.cpp
+++ src/dcastle.cpp
@@ -7,10 +7,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <netinet/in.h> // for in_port_t
 
 // System C++ headers
 #include <sstream>
 #include <iostream>
+#include <vector>
 
 // DC headers
 #include <fileinfo.h>
@@ -32,37 +34,48 @@ int scheck = 0; /* for syntax checking mode */
 uint16_t DFLT_PORT = 6667, DFLT_PORT2 = 6666, DFLT_PORT3 = 4000, DFLT_PORT4 = 6669;
 CVoteData *DCVote;
 
+struct config {
+  vector<in_port_t> ports;
+  bool allow_imp_password;
+  bool verbose_mode;
+  bool test_mobs;
+  bool test_objs;
+  bool test_world;
+  bool bport;
+  bool check_syntax;
+  char *dir;
+};
+
 void init_random();
 void init_game(void);
 void boot_zones(void);
 void boot_world(void);
+void backup_executable(char * const argv[]);
+config parse_arguments(int argc, char * const argv[]);
 
 /**********************************************************************
  *  main game loop and related stuff                                  *
  **********************************************************************/
 
-int main(int argc, char **argv) {
+int main(int argc, char * const argv[]) {
 	struct stat stat_buffer;
 	char char_buffer[512] = { '\0' };
 
 	logf(0, LOG_MISC, "Executable: %s Version: %s Build date: %s\n", argv[0], DC::getVersion().c_str(), DC::getBuildTime().c_str());
 
-#ifndef __CYGWIN__
-	// Make a copy of our executable so that in the event of a crash we have a
-	// known good copy to debug with.
-	stringstream backup_filename, cmd;
-	backup_filename << argv[0] << "." << getpid();
+	backup_executable(argv);
+	
+	config cf = parse_arguments(argc, argv);
 
-	// If backup file does not exist already then link to it
-	if (stat(backup_filename.str().c_str(), &stat_buffer) == -1) {
-		if (link(argv[0], backup_filename.str().c_str()) == -1) {
-			const char * strerror_result = strerror_r(errno, char_buffer, sizeof(char_buffer));
-			logf(0, LOG_MISC, "Error linking %s to %s: %s", argv[0], backup_filename.str().c_str(), strerror_result);
-		}
+	vector<in_port_t>::iterator i = cf.ports.begin();
+	while(i != cf.ports.end()) {
+	  cerr << *i << ", ";
+	  i++;
 	}
-#endif
-
-	orig_argv = argv;
+	
+	exit(0);
+	
+	//	orig_argv = argv;
 	int pos = 1;
 	char dir[256] = { '\0' };
 	strncpy(dir, (char *) DFLT_DIR, 255);
@@ -77,81 +90,7 @@ int main(int argc, char **argv) {
 
 	while ((pos < argc) && (*(argv[pos]) == '-')) {
 		switch (*(argv[pos] + 1)) {
-		case 'v':
-			verbose_mode = 1;
-			break;
-		case 'h':
-		case '?':
-			fprintf(stderr,
-					"Usage: %s [-v] [-h] [-w] [-c] [-m] [-d directory] [-p] [-P]\n"
-							"-v\t\tVerbose mode\n"
-							"-h\t\tUsage information\n"
-							"-w\t\tWorld testing mode\n"
-							"-c\t\tObj testing mode\n"
-							"-m\t\tObj & Mob testing mode\n"
-							"-b\t\tBuilders' port (7000-7003)\n"
-							"-d directory\tData directory\n"
-							"-p\t\tUse port 1500-1503\n"
-							"-P\t\tAllow imps to use their password\n\n"
-							"-s\t\tSyntax checking mode\n",
-					argv[0]);
 
-			return 0;
-			break;
-		case 'w':
-			code_testing_mode = 1;
-			code_testing_mode_world = 1;
-			log("Mud in world checking mode. TinyTinyworld being used. (WLD)",
-					0, LOG_MISC);
-			log("Do NOT have mortals login when in world checking mode.", 0,
-					LOG_MISC);
-			break;
-
-		case 'c':
-			code_testing_mode = 1;
-			log("Mud in testing mode. TinyTinyworld being used. (OBJ)", 0,
-					LOG_MISC);
-			break;
-
-		case 'm':
-			code_testing_mode = 1;
-			code_testing_mode_mob = 1;
-			log("Mud in testing mode. TinyTinyworld being used. (MOB,OBJ)", 0,
-					LOG_MISC);
-			break;
-		case 'b': // Buildin' port.
-			bport = 1;
-			port1 = 7000;
-			port2 = 7001;
-			port3 = 7002;
-			port4 = 7003;
-			break;
-		case 'd':
-			if (argv[pos][2] != '\0')
-				strcpy(dir, &argv[pos][2]);
-			else if (++pos < argc)
-				strcpy(dir, &argv[pos][0]);
-			else {
-				fprintf(stderr, "Directory arg expected after -d.\n\r");
-				exit(1);
-			}
-			break;
-		case 'p':
-			port1 = 1500;
-			port2 = 1501;
-			port3 = 1502;
-			port4 = 1503;
-			break;
-		case 'P':
-			allow_imp_password = true;
-			break;
-		case 's':
-			scheck = true;
-			break;
-		default:
-			logf(0, LOG_MISC, "SYSERR: Unknown option -%c in argument string.",
-					*(argv[pos] + 1));
-			break;
 		}
 		pos++;
 	}
@@ -159,11 +98,8 @@ int main(int argc, char **argv) {
 	if (pos < argc) {
 		if (!isdigit(*argv[pos])) {
 			fprintf(stderr,
-					"Usage: %s [-c] [-m] [-q] [-r] [-s] [-d pathname] [port #]\n",
+					"Usage: %s [-v] [-h] [-w] [-c] [-m] [-d directory] [-p] [-P] [port #]\n",
 					argv[0]);
-			exit(1);
-		} else if ((port1 = atoi(argv[pos])) <= 1024) {
-			fprintf(stderr, "Illegal port number.\n");
 			exit(1);
 		}
 	}
@@ -174,7 +110,8 @@ int main(int argc, char **argv) {
 		port4 = port1 + 3;
 	}
 #ifdef WIN32
-	for(unsigned i = 0; i < strlen(dir); i++)
+cd
+  cdfor(unsigned i = 0; i < strlen(dir); i++)
 	{
 		if(dir[i] == '/')
 		{
@@ -217,3 +154,96 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+void backup_executable(char * const argv[])
+{
+#ifndef __CYGWIN__
+	struct stat stat_buffer;
+	char char_buffer[512] = { '\0' };
+	
+	// Make a copy of our executable so that in the event of a crash we have a
+	// known good copy to debug with.
+	stringstream backup_filename, cmd;
+	backup_filename << argv[0] << "." << getpid();
+
+	// If backup file does not exist already then link to it
+	if (stat(backup_filename.str().c_str(), &stat_buffer) == -1) {
+		if (link(argv[0], backup_filename.str().c_str()) == -1) {
+			const char * strerror_result = strerror_r(errno, char_buffer, sizeof(char_buffer));
+			logf(0, LOG_MISC, "Error linking %s to %s: %s", argv[0], backup_filename.str().c_str(), strerror_result);
+		}
+	}
+#endif
+	return;
+}
+
+config parse_arguments(int argc, char * const argv[])
+{
+  char opt;
+  uint32_t port;
+  config cf;
+
+  while ((opt = getopt(argc, argv, "vp:Pmbdshwc?")) != -1) {
+	switch (opt) {
+	case 'v':
+	  cf.verbose_mode = true;
+	  break;
+	case 'p':
+	  port = atoi(optarg);
+	  cf.ports.push_back(port);
+	  break;
+	case 'P':
+	  cf.allow_imp_password = true;
+	  break;
+	case 'm':
+	  cf.test_mobs = 1;
+	  cf.test_objs = 1;
+	  log("Mud in testing mode. TinyTinyworld being used. (MOB,OBJ)", 0, LOG_MISC);
+	  break;
+	case 'b': // Buildin' port.
+	  cf.bport = 1;
+	  cf.ports.push_back(7000);
+	  cf.ports.push_back(7001);
+	  cf.ports.push_back(7002);
+	  cf.ports.push_back(7003);
+	  break;
+	case 'd':
+	  cf.dir = optarg;
+	  break;
+	case 's':
+	  cf.check_syntax = true;
+	  break;
+	case 'w':
+	  cf.test_world = 1;
+	  log("Mud in world checking mode. TinyTinyworld being used. (WLD)",
+		  0, LOG_MISC);
+	  log("Do NOT have mortals login when in world checking mode.", 0,
+		  LOG_MISC);
+	  break;
+	case 'c':
+	  cf.test_objs = 1;
+	  log("Mud in testing mode. TinyTinyworld being used. (OBJ)", 0, LOG_MISC);
+	  break;
+	default:
+	case 'h':
+	case '?':
+	  fprintf(stderr,
+			  "Usage: %s [-v] [-h] [-w] [-c] [-m] [-d directory] [-p port#] [-P]\n"
+			  "-v\t\tVerbose mode\n"
+			  "-h\t\tUsage information\n"
+			  "-w\t\tWorld testing mode\n"
+			  "-c\t\tObj testing mode\n"
+			  "-m\t\tObj & Mob testing mode\n"
+			  "-b\t\tBuilders' port (7000-7003)\n"
+			  "-d directory\tData directory\n"
+			  "-p [port#]\tCan be repeated to listen on multiple ports\n"
+			  "-P\t\tAllow imps to use their password\n\n"
+			  "-s\t\tSyntax checking mode\n",
+			  argv[0]);
+	  
+	  exit(EXIT_FAILURE);
+	  break;
+	}
+  }
+
+  return cf;
+}
