65e8ae417ae55678f525522ce9660925be7d6a4f
Paul Koos <paully_k5298@yahoo.com>
2022-02-25 10:09:59 -0800

    include RoomFlags header in room.

--- src/room.h
+++ src/room.h
@@ -6,6 +6,7 @@
 | Description:  This file contains all of the room header file/constant
 |   information.  It also contains information about the 'world' structs.
 */
+#include "RoomFlags.h"
 #include "structs.h" // ubyte
 #include "obj.h" // ubyte
 #include "MobActivity.h" // struct path_data
@@ -15,41 +16,7 @@
 
 const unsigned int NOWHERE = 0;
 
-/* Bitvector For 'room_flags' */
 
-#define DARK         1
-#define NOHOME        1<<1    
-#define NO_MOB       1<<2
-#define INDOORS      1<<3
-#define TELEPORT_BLOCK  1<<4
-#define NO_KI      1<<5
-#define NOLEARN      1<<6
-#define NO_MAGIC     1<<7
-#define TUNNEL       1<<8
-#define PRIVATE      1<<9
-#define SAFE         1<<10
-#define NO_SUMMON    1<<11
-#define NO_ASTRAL    1<<12    // usused
-#define NO_PORTAL    1<<13
-#define IMP_ONLY     1<<14
-#define FALL_DOWN    1<<15
-#define ARENA        1<<16
-#define QUIET        1<<17
-#define UNSTABLE     1<<18
-#define NO_QUIT      1<<19
-#define FALL_UP      1<<20
-#define FALL_EAST    1<<21
-#define FALL_WEST    1<<22
-#define FALL_SOUTH   1<<23
-#define FALL_NORTH   1<<24
-#define NO_TELEPORT  1<<25
-#define NO_TRACK     1<<26
-#define CLAN_ROOM    1<<27
-#define NO_SCAN      1<<28
-#define NO_WHERE     1<<29
-#define LIGHT_ROOM   1<<30
-//#define NOHOME       1<<31
-//#define NO_KI        1<<31
 
 /* Bitvector For 'temp_room_flags' 
    temp_room_flags are NOT in the world file and cannot be saved or added at boot time
