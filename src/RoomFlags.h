a7d84c3364291ab21e4f0d7a51b9544f12b929a6
Paul Koos <paully_k5298@yahoo.com>
2022-02-25 10:09:36 -0800

    add RoomFlags file.

---
+++ src/RoomFlags.h
@@ -0,0 +1,36 @@
+#ifndef ROOM_FLAGS_H_
+#define ROOM_FLAGS_H_
+
+#define DARK            1
+#define NOHOME          1<<1
+#define NO_MOB          1<<2
+#define INDOORS         1<<3
+#define TELEPORT_BLOCK  1<<4
+#define NO_KI           1<<5
+#define NOLEARN         1<<6
+#define NO_MAGIC        1<<7
+#define TUNNEL          1<<8
+#define PRIVATE         1<<9
+#define SAFE            1<<10
+#define NO_SUMMON       1<<11
+#define NO_ASTRAL       1<<12    // usused
+#define NO_PORTAL       1<<13
+#define IMP_ONLY        1<<14
+#define FALL_DOWN       1<<15
+#define ARENA           1<<16
+#define QUIET           1<<17
+#define UNSTABLE        1<<18
+#define NO_QUIT         1<<19
+#define FALL_UP         1<<20
+#define FALL_EAST       1<<21
+#define FALL_WEST       1<<22
+#define FALL_SOUTH      1<<23
+#define FALL_NORTH      1<<24
+#define NO_TELEPORT     1<<25
+#define NO_TRACK        1<<26
+#define CLAN_ROOM       1<<27
+#define NO_SCAN         1<<28
+#define NO_WHERE        1<<29
+#define LIGHT_ROOM      1<<30
+
+#endif
