5df223cf9f8e7b4e0cd3c951bc821c10c576c28b
Paul Koos <paully_k5298@yahoo.com>
2022-02-25 10:14:51 -0800

    add Directions to header file and include in room.

---
+++ src/ExitInfo.h
@@ -0,0 +1,14 @@
+#ifndef EXIT_INFO_H_
+#define EXIT_INFO_H_
+
+/* For exit_info */
+
+#define EX_ISDOOR       1
+#define EX_CLOSED       2
+#define EX_LOCKED       4
+#define EX_HIDDEN       8
+#define EX_IMM_ONLY    16
+#define EX_PICKPROOF   32
+#define EX_BROKEN      64
+
+#endif
--- src/room.h
+++ src/room.h
@@ -6,6 +6,7 @@
 | Description:  This file contains all of the room header file/constant
 |   information.  It also contains information about the 'world' structs.
 */
+#include "ExitInfo.h"
 #include "Direction.h"
 #include "RoomFlags.h"
 #include "structs.h" // ubyte
@@ -28,16 +29,6 @@ const unsigned int NOWHERE = 0;
 /* Internal flags */
 #define iNO_TRACK   1
 #define iNO_MAGIC   1<<1
-
-/* For exit_info */
-
-#define EX_ISDOOR       1
-#define EX_CLOSED       2
-#define EX_LOCKED       4
-#define EX_HIDDEN       8
-#define EX_IMM_ONLY    16
-#define EX_PICKPROOF   32
-#define EX_BROKEN      64
 
 /* For 'Sector types' */
 
