3900daecf2ba116872564620b4d0080500603e60
Paul Koos <paully_k5298@yahoo.com>
2022-02-25 10:16:37 -0800

    add SectorTypes to header file and include in room.

---
+++ src/SectorTypes.h
@@ -0,0 +1,25 @@
+#ifndef SECTOR_TYPES_H_
+#define SECTOR_TYPES_H_
+
+/* For 'Sector types' */
+
+#define SECT_INSIDE           0
+#define SECT_CITY             1
+#define SECT_FIELD            2
+#define SECT_FOREST           3
+#define SECT_HILLS            4
+#define SECT_MOUNTAIN         5
+#define SECT_WATER_SWIM       6
+#define SECT_WATER_NOSWIM     7
+#define SECT_BEACH            8
+#define SECT_PAVED_ROAD       9
+#define SECT_DESERT          10
+#define SECT_UNDERWATER      11
+#define SECT_SWAMP           12
+#define SECT_AIR             13
+#define SECT_FROZEN_TUNDRA   14
+#define SECT_ARCTIC          15
+#define SECT_MAX_SECT        15 // update this if you add more
+                                // and const.c stuff for sectors
+
+#endif
