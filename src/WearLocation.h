22f4762c3b6eb86f06b5b5a7f0b028442ab66de9
Paul Koos <paully_k5298@yahoo.com>
2022-02-25 22:06:37 -0800

    move WearLocation to separate header.

---
+++ src/WearLocation.h
@@ -0,0 +1,26 @@
+#ifndef WEAR_LOCATION_H_
+#define WEAR_LOCATION_H_
+
+/* Bitvector For 'wear_flags' */
+
+#define ITEM_TAKE               1
+#define ITEM_WEAR_FINGER        2
+#define ITEM_WEAR_NECK          4
+#define ITEM_WEAR_BODY          8
+#define ITEM_WEAR_HEAD         16
+#define ITEM_WEAR_LEGS         32
+#define ITEM_WEAR_FEET         64
+#define ITEM_WEAR_HANDS       128
+#define ITEM_WEAR_ARMS        256
+#define ITEM_WEAR_SHIELD      512
+#define ITEM_WEAR_ABOUT      1024
+#define ITEM_WEAR_WAISTE     2048
+#define ITEM_WEAR_WRIST      4096
+#define ITEM_WIELD           8192
+#define ITEM_HOLD           16384
+#define ITEM_THROW          32768
+#define ITEM_LIGHT_SOURCE   65536
+#define ITEM_WEAR_FACE     131072
+#define ITEM_WEAR_EAR      262144
+
+#endif
