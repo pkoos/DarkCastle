26afcb4ddafa890827ee2af9eb10375d2010ebd1
Paul Koos <paully_k5298@yahoo.com>
2022-02-26 09:53:42 -0800

    add MessageDestination to separate header.

--- src/act.h
+++ src/act.h
@@ -7,6 +7,7 @@
 | Description:  This file contains the header information for the new
 |   act() function
 */
+#include "MessageDisplay.h"
 #include "MessageDestination.h"
 #include <string>
 #include "character.h"
@@ -37,24 +38,5 @@ struct send_tokens_return
 send_tokens_return send_tokens(TokenList * tokens, CHAR_DATA *ch, OBJ_DATA * obj, void * vch, int flags, CHAR_DATA *to);
 
 void send_message(const char *str, CHAR_DATA *to);
-
-//--
-// Constants
-//--
-// Undefine the old ones if this header is included
-// #undef TO_ROOM
-// #undef TO_VICT
-// #undef TO_CHAR
-
-// These constants go in the flags part (optional -- 0 for none)
-#define DEFAULT       0    // "someone" if invisible, sleepers skipped
-#define NOTVICT       1<<0 // Sends to destination except victim
-#define GODS          1<<1 // Sends to destination, gods only
-#define ASLEEP        1<<2 // Will send even to sleepers
-#define INVIS_NULL    1<<3 // Invisible messages are skipped completely
-#define INVIS_VISIBLE 1<<4 // Invisible messages are shown w/names visible
-#define FORCE         1<<5 // Sends regardless of nanny state
-#define STAYHIDE      1<<6 // Stayhide flag keeps thieves in hiding.
-#define BARDSONG      1<<7 // Bard song so only show it to people in room with BARD_SONG toggle set to verbose
 
 #endif /* ACT_H_ */
