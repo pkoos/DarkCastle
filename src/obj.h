07604022de7ef6bf84dc964d31432ae2375c2033
Paul Koos <paully_k5298@yahoo.com>
2022-02-25 22:00:19 -0800

    add header for ItemType.

--- src/obj.h
+++ src/obj.h
@@ -16,48 +16,11 @@
 #ifndef OBJ_H_
 #define OBJ_H_
 
+#include "ItemType.h"
 #include "structs.h" // ubyte
 #include "character.h"
 
 /* The following defs are for obj_data  */
-
-/* For 'type_flag' */
-
-#define ITEM_LIGHT       1
-#define ITEM_SCROLL      2
-#define ITEM_WAND        3
-#define ITEM_STAFF       4
-#define ITEM_WEAPON      5
-#define ITEM_FIREWEAPON  6
-#define ITEM_MISSILE     7
-#define ITEM_TREASURE    8
-#define ITEM_ARMOR       9
-#define ITEM_POTION     10
-#define ITEM_WORN       11   // not used, can change
-#define ITEM_OTHER      12
-#define ITEM_TRASH      13
-#define ITEM_TRAP       14 
-#define ITEM_CONTAINER  15
-#define ITEM_NOTE       16
-#define ITEM_DRINKCON   17
-#define ITEM_KEY        18
-#define ITEM_FOOD       19
-#define ITEM_MONEY      20
-#define ITEM_PEN        21
-#define ITEM_BOAT       22
-#define ITEM_BOARD      23
-#define ITEM_PORTAL     24
-#define ITEM_FOUNTAIN   25
-#define ITEM_INSTRUMENT 26
-#define ITEM_UTILITY    27
-#define ITEM_BEACON     28 
-#define ITEM_LOCKPICK   29
-#define ITEM_CLIMBABLE  30
-#define ITEM_MEGAPHONE  31
-#define ITEM_ALTAR	    32
-#define ITEM_TOTEM	    33
-#define ITEM_KEYRING    34
-#define ITEM_TYPE_MAX   34
 
 /* Bitvector For 'wear_flags' */
 
