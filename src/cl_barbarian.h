1f7c3b2537634049deb3a362770153d62e9a1ed0
Paul Koos <paully_k5298@yahoo.com>
2022-03-02 15:37:39 -0800

    move method barbarian declarations from interp header to barbarian header.

--- src/cl_barbarian.h
+++ src/cl_barbarian.h
@@ -28,6 +28,22 @@ extern CWorld world;
 int attempt_move(CHAR_DATA *ch, int cmd, int is_retreat = 0);
 int find_door(CHAR_DATA *ch, char *type, char *dir);
 
+int do_batter(CHAR_DATA *ch, char *argument, int cmd);
+int do_brace(CHAR_DATA *ch, char *argument, int cmd);
+int do_rage(CHAR_DATA *ch, char *argument, int cmd);
+int do_battlecry(CHAR_DATA *ch, char *argument, int cmd);
+int do_berserk(CHAR_DATA *ch, char *argument, int cmd);
+int do_headbutt(CHAR_DATA *ch, char *argument, int cmd);
+int do_bloodfury(CHAR_DATA *ch, char *argument, int cmd);
+int do_crazedassault(CHAR_DATA *ch, char *argument, int cmd);
+void rush_reset(void *arg1, void *arg2, void *arg3);
+int do_bullrush(CHAR_DATA *ch, char *argument, int cmd);
+int do_ferocity(CHAR_DATA *ch, char *argument, int cmd);
+void barb_magic_resist(char_data *ch, int old, int nw);
+int do_knockback(CHAR_DATA *ch, char *argument, int cmd);
+int do_primalfury(CHAR_DATA *ch, char *argument, int cmd);
+int do_pursue(CHAR_DATA *ch, char *argument, int cmd);
+
 // barbarian 10007 guildmaster - done and checked, Apoc
 struct class_skill_defines b_skills[] = { // barbarian skills
 
